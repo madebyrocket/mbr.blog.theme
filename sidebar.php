c394a97cfc001851112a1b2d744c8d0fa899d120
bricooke <brian@madebyrocket.com>
2010-05-27 06:01:40 -0600

    add rss link and more cleanup to sidebar (assume dynamic sidebars).

--- sidebar.php
+++ sidebar.php
@@ -1,40 +1,20 @@
 <!-- begin sidebar -->
 <div id="sidebar">
-	<ul id="sb1">
-	<?php if ( function_exists('dynamic_sidebar') && dynamic_sidebar('Sidebar 1') ) : else : ?>
-
-	<?php if((function_exists('is_front_page') && !is_front_page()) && !is_home())/*link to home */{ ?>
-		<li id="homelink"><a href="<?php bloginfo('url'); ?>/"><?php _e('&laquo; Back To Blog','basic2col'); ?></a></li>
-	<?php } ?>
-
-
-	<li id="search">
-	<form id="searchform" method="get" action="<?php bloginfo('url') ?>">
-		<div>
-			<input type="text" value="" name="s" id="s" />
-			<input type="submit" class="submit" accesskey="s" value="<?php _e('Search','basic2col'); ?>" />
-		</div>
-		</form>
+        <ul id="sb1">
+        <?php dynamic_sidebar('Sidebar 1'); ?>
+        </ul>
+        
+        <ul id="sb2">
+	<?php dynamic_sidebar('Sidebar 2'); ?>
+	<li>
+	<a href="<?php bloginfo('rss2_url'); ?>">
+	<img src="<?php bloginfo('template_url')?>/gfx/rss.png" alt="RSS" class="rssimg" /> 
+	RSS Feed
+	</a>
 	</li>
-
-
-	<?php wp_list_pages('sort_column=menu_order&title_li=<h3>' . __('Pages','basic2col') . '</h3>'); ?>
-
-
-	
-	<?php wp_list_categories('sort_column=name&show_count=1&title_li=<h3>' . __('Categories','basic2col') . '</h3>'); ?>
-	<?php endif; ?>
-	</ul>
-
-
-
-	<ul id="sb2">
-		<?php if ( function_exists('dynamic_sidebar') && dynamic_sidebar('Sidebar 2') ) : else : ?>
-
-
-
-        <?php endif; ?>
+        </ul>
     </ul>
 
 </div>
 <!-- end sidebar -->
+
